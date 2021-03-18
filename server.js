b6308997522ad7a93a4fb58aaa91ca6f665ed86c
putraaaaaaaaaaaa <75620395+putraaaaaaaaaaaa@users.noreply.github.com>
2021-03-19 02:16:44 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const { Collection, Client, Discord, MessageEmbed } = require("discord.js");
+const fs = require("fs");
+const client = new Client
+
+module.exports = client;
+
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+["command"].forEach(handler => {
+  require(`./handlers/${handler}`)(client);
+  });
+
+client.login('Token')
